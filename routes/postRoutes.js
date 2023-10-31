0ac68a93a8347b18bb939748df6b467e865733a1
Sakshi Shukla <92422453+sakshis-12@users.noreply.github.com>
2023-10-31 18:13:00 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,18 +1,21 @@
-
 const express = require("express");
 const router = express.Router();
 const { getPosts, createPost, updatePost, deletePost, getPost, getLatestPosts} = require("../controllers/postController")
 
-router.route("/").get(getPosts);
+router.get("/", (req, res) => {
+    res.send("HOME PAGE");
+  });
 
-router.route("/:id").get(getPost)
+router.route("/api/posts").get(getPosts);
 
-router.route("/").post(createPost)
+router.route("/api/posts:id").get(getPost)
 
-router.route("/:id").put(updatePost)
+router.route("/api/posts").post(createPost)
 
-router.route("/:id").delete(deletePost)
+router.route("/api/posts:id").put(updatePost)
 
-router.route("/").get(getLatestPosts)
+router.route("/api/posts:id").delete(deletePost)
+
+router.route("/api/posts/latest").get(getLatestPosts)
 
 module.exports = router;
